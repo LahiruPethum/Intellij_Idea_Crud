877d5c03ae1379d43785c8725dd12bf6a7e4cacf
Lahiru Pethum <pethum013@gmail.com>
2022-10-03 15:20:57 +0530

    second commit

--- src/CRUD.java
+++ src/CRUD.java
@@ -1,2 +1,13 @@
+import javax.swing.*;
+
 public class CRUD {
+    private JPanel Main;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CRUD");
+        frame.setContentPane(new CRUD().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
